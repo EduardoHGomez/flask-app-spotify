88f27db7f5740753b0727f7d54907ed6e3f215a1
Eduardo <hurtado.josegomez@gmail.com>
2024-04-28 13:46:09 -0600

    rollback

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+import pandas as pd
+import numpy as np
+
+from analize import getUserMetrics
+
+from flask import Flask, render_template, request, jsonify, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route("/redirect")
+def redirect():
+    return "Redirect link"
+
+@app.route('/analize', methods=['POST'])
+def analize():
+    auth = request.headers.get('Authorization')
+
+    data = request.get_json()  # Parse JSON
+    tracks = data['array']  # Get array
+
+    # Do the analysis
+    result = getUserMetrics(tracks, auth)
+
+    return jsonify(result)
+
+
+@app.route("/results", methods=['GET'])
+def results():
+    return render_template('results.html')
+
+@app.route("/")
+def home():
     return render_template('index.html')
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
